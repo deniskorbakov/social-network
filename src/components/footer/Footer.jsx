edb2ba3d07db7745e9d89b4666c10eab92347021
deniskorbakov <korbakovd@gmail.com>
2023-04-22 22:53:25 +0300

    изменил абзацы на ссылки

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,16 +3,16 @@ function Footer() {
         <footer className="container mx-auto p-2 border-t border-white mt-5">
             <div className="flex text-center pt-2">
                 <div className="flex-auto w-8">
-                    <p className="font-bold text-lg text-white">Social Network</p>
+                    <a className="font-bold text-lg text-white" href="/profile">Social Network</a>
                 </div>
                 <div className="flex-auto w-8">
-                    <p className="font-bold text-lg text-white">Home</p>
+                    <a className="font-bold text-lg text-white" href="/dialogs">Home</a>
                 </div>
                 <div className="flex-auto w-8">
-                    <p className="font-bold text-lg text-white">About Us</p>
+                    <a className="font-bold text-lg text-white">About Us</a>
                 </div>
                 <div className="flex-auto w-8">
-                    <p className="font-bold text-lg text-white">Contact</p>
+                    <a className="font-bold text-lg text-white">Contact</a>
                 </div>
             </div>
         </footer>
