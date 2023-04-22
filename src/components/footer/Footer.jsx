30d1f5861161470bd184d608ca0fc11ca0116d50
deniskorbakov <korbakovd@gmail.com>
2023-04-23 00:19:52 +0300

    изменил на навлинк

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,14 @@
+import {NavLink} from "react-router-dom";
+
 function Footer() {
     return (
         <footer className="container mx-auto p-2 border-t border-white mt-5">
             <div className="flex text-center pt-2">
                 <div className="flex-auto w-8">
-                    <a className="font-bold text-lg text-white" href="/profile">Social Network</a>
+                    <NavLink className="font-bold text-lg text-white aria-[current=page]:text-cyan-300 border-2 rounded-full px-2 hover:text-base" to="/profile">Social Network</NavLink>
                 </div>
                 <div className="flex-auto w-8">
-                    <a className="font-bold text-lg text-white" href="/dialogs">Home</a>
+                    <NavLink className="font-bold text-lg text-white aria-[current=page]:text-cyan-300 border-2 rounded-full px-2 hover:text-base" to="/dialogs" >Home</NavLink>
                 </div>
                 <div className="flex-auto w-8">
                     <a className="font-bold text-lg text-white">About Us</a>
