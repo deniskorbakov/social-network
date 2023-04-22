5007c3a8c84592f65e63065263b08d9eed4d61ca
deniskorbakov <korbakovd@gmail.com>
2023-04-22 22:54:02 +0300

    сделал роутинг на страницы

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,24 @@
 import '../style/App.scss'
-import Main from "./main/Main.jsx";
 import Header from "./header/Header.jsx";
 import Footer from "./footer/Footer.jsx";
+import Profile from "./main/profile/Profile.jsx";
+import Dialogs from "./main/dialogs/Dialogs.jsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
     return (
-        <div className="bg-gradient-to-r from-indigo-800 to-cyan-500">
-            <Header/>
-            <Main/>
-            <Footer/>
-        </div>
+        <BrowserRouter>
+            <div className="bg-gradient-to-r from-indigo-800 to-cyan-500">
+                <Header/>
+                <main className="shadow-2xl rounded bg-gradient-to-r from-indigo-800 to-cyan-500">
+                    <Routes>
+                        <Route path='/profile' element={<Profile/>}/>
+                        <Route path='/dialogs' element={<Dialogs/>}/>
+                    </Routes>
+                </main>
+                <Footer/>
+            </div>
+        </BrowserRouter>
     );
 }
 
