9b776748823b149dc784cba204610f1cb088fe97
deniskorbakov <korbakovd@gmail.com>
2023-04-23 01:29:56 +0300

    добавил дизайн диалогов

--- src/components/main/dialogs/Dialogs.jsx
+++ src/components/main/dialogs/Dialogs.jsx
@@ -1,7 +1,123 @@
 function Dialogs() {
     return (
-        <div>
-            dialogs
+        <div className="container mx-auto mt-5 p-5 text-center h-screen">
+            <div className="container mx-auto text-center mt-5">
+                <h1 className="font-bold text-3xl text-white uppercase">My Dialog</h1>
+            </div>
+
+            <div className="flex border border-white rounded-md mt-11">
+                <div className="flex-initial w-96 border-r">
+                    <div className="container text-center border-b p-2">
+                        <p className="text-2xl font-bold text-white">Dialogs</p>
+                    </div>
+
+                    <div className="text-center p-1">
+                        <div className="p-6 bg-white shadow-lg flex items-center space-x-4 w-full drop-shadow-md border-b hover:bg-gray-200">
+                            <div className="shrink-0">
+                                <img className="h-12 w-12 rounded-full" src="https://cdnn1.img.sputniknews.com/img/102062/98/1020629890_18:0:6469:3648_1920x0_80_0_0_0d1dd4223c23b0a4c2e27b7f9d7f9b1c.jpg" alt="ChitChat Logo"/>
+                            </div>
+                            <div>
+                                <div className="text-xl font-medium text-black">ChitChat</div>
+                                <p className="text-slate-500">You have a new message!</p>
+                            </div>
+                        </div>
+
+                        <div className="p-6 bg-white shadow-lg flex items-center space-x-4 w-full drop-shadow-md border-b hover:bg-gray-200">
+                            <div className="shrink-0">
+                                <img className="h-12 w-12 rounded-full" src="https://cdnn1.img.sputniknews.com/img/102062/98/1020629890_18:0:6469:3648_1920x0_80_0_0_0d1dd4223c23b0a4c2e27b7f9d7f9b1c.jpg" alt="ChitChat Logo"/>
+                            </div>
+                            <div>
+                                <div className="text-xl font-medium text-black">ChitChat</div>
+                                <p className="text-slate-500">You have a new message!</p>
+                            </div>
+                        </div>
+
+                        <div className="p-6 bg-white shadow-lg flex items-center space-x-4 w-full drop-shadow-md border-b hover:bg-gray-200">
+                            <div className="shrink-0">
+                                <img className="h-12 w-12 rounded-full" src="https://cdnn1.img.sputniknews.com/img/102062/98/1020629890_18:0:6469:3648_1920x0_80_0_0_0d1dd4223c23b0a4c2e27b7f9d7f9b1c.jpg" alt="ChitChat Logo"/>
+                            </div>
+                            <div>
+                                <div className="text-xl font-medium text-black">ChitChat</div>
+                                <p className="text-slate-500">You have a new message!</p>
+                            </div>
+                        </div>
+
+                        <div className="p-6 bg-white shadow-lg flex items-center space-x-4 w-full drop-shadow-md border-b hover:bg-gray-200">
+                            <div className="shrink-0">
+                                <img className="h-12 w-12 rounded-full" src="https://cdnn1.img.sputniknews.com/img/102062/98/1020629890_18:0:6469:3648_1920x0_80_0_0_0d1dd4223c23b0a4c2e27b7f9d7f9b1c.jpg" alt="ChitChat Logo"/>
+                            </div>
+                            <div>
+                                <div className="text-xl font-medium text-black">ChitChat</div>
+                                <p className="text-slate-500">You have a new message!</p>
+                            </div>
+                        </div>
+
+                        <div className="p-6 bg-white shadow-lg flex items-center space-x-4 w-full drop-shadow-md border-b hover:bg-gray-200">
+                            <div className="shrink-0">
+                                <img className="h-12 w-12 rounded-full" src="https://cdnn1.img.sputniknews.com/img/102062/98/1020629890_18:0:6469:3648_1920x0_80_0_0_0d1dd4223c23b0a4c2e27b7f9d7f9b1c.jpg" alt="ChitChat Logo"/>
+                            </div>
+                            <div>
+                                <div className="text-xl font-medium text-black">ChitChat</div>
+                                <p className="text-slate-500">You have a new message!</p>
+                            </div>
+                        </div>
+
+                        <div className="p-6 bg-white shadow-lg flex items-center space-x-4 w-full drop-shadow-md border-b hover:bg-gray-200">
+                            <div className="shrink-0">
+                                <img className="h-12 w-12 rounded-full" src="https://cdnn1.img.sputniknews.com/img/102062/98/1020629890_18:0:6469:3648_1920x0_80_0_0_0d1dd4223c23b0a4c2e27b7f9d7f9b1c.jpg" alt="ChitChat Logo"/>
+                            </div>
+                            <div>
+                                <div className="text-xl font-medium text-black">ChitChat</div>
+                                <p className="text-slate-500">You have a new message!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex-initial w-full">
+                    <div className="text-center p-4">
+                        <div className="p-6 flex items-center space-x-4 w-full">
+                            <div className="shrink-0">
+                                <img className="h-12 w-12 rounded-full" src="https://cdnn1.img.sputniknews.com/img/102062/98/1020629890_18:0:6469:3648_1920x0_80_0_0_0d1dd4223c23b0a4c2e27b7f9d7f9b1c.jpg" alt="ChitChat Logo"/>
+                            </div>
+                            <div>
+                                <div className="text-xl font-medium text-white text-left">Denis korbakov</div>
+                                <p className="text-gray-300">You have a new message!</p>
+                            </div>
+                        </div>
+
+                        <div className="p-6 flex items-center space-x-4 w-full">
+                            <div className="shrink-0">
+                                <img className="text-right h-12 w-12 rounded-full" src="https://cdnn1.img.sputniknews.com/img/102062/98/1020629890_18:0:6469:3648_1920x0_80_0_0_0d1dd4223c23b0a4c2e27b7f9d7f9b1c.jpg" alt="ChitChat Logo"/>
+                            </div>
+                            <div>
+                                <div className="text-xl font-medium text-white text-left">Me</div>
+                                <p className="text-gray-300">invite to gym</p>
+                            </div>
+                        </div>
+
+                        <div className="p-6 flex items-center space-x-4 w-full">
+                            <div className="shrink-0">
+                                <img className="text-right h-12 w-12 rounded-full" src="https://cdnn1.img.sputniknews.com/img/102062/98/1020629890_18:0:6469:3648_1920x0_80_0_0_0d1dd4223c23b0a4c2e27b7f9d7f9b1c.jpg" alt="ChitChat Logo"/>
+                            </div>
+                            <div>
+                                <div className="text-xl font-medium text-white text-left">Me</div>
+                                <p className="text-gray-300">invite to gym</p>
+                            </div>
+                        </div>
+
+                        <div className="p-6 flex items-center space-x-4 w-full">
+                            <div className="shrink-0">
+                                <img className="text-right h-12 w-12 rounded-full" src="https://cdnn1.img.sputniknews.com/img/102062/98/1020629890_18:0:6469:3648_1920x0_80_0_0_0d1dd4223c23b0a4c2e27b7f9d7f9b1c.jpg" alt="ChitChat Logo"/>
+                            </div>
+                            <div>
+                                <div className="text-xl font-medium text-white text-left">Me</div>
+                                <p className="text-gray-300">invite to gym</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
