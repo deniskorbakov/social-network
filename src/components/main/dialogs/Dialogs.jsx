38301b8f82602e2aff85cd9922dd3c781e9d70f1
deniskorbakov <korbakovd@gmail.com>
2023-04-24 14:34:45 +0300

    данные вынес в стейт

--- src/components/main/dialogs/Dialogs.jsx
+++ src/components/main/dialogs/Dialogs.jsx
@@ -1,63 +1,11 @@
-import {NavLink} from "react-router-dom";
+import DialogItem from "./dialogItem/DialogItem.jsx";
+import Message from "./message/Message.jsx";
 
-let dialogs = [
-    {id: 1, name: "denis", message: "hi"},
-    {id: 2, name: "dima", message: "hello"},
-    {id: 2, name: "dima", message: "hello"},
-    {id: 2, name: "dima", message: "hello"},
-    {id: 2, name: "dima", message: "hello"},
-    {id: 2, name: "dima", message: "hello"},
-];
 
-let messages = [
-    {name: "denis", message: "hi"},
-    {name: "dima", message: "hello"},
-    {name: "dima", message: "hello"},
-    {name: "dima", message: "hello"},
-    {name: "dima", message: "hello"},
-    {name: "dima", message: "hello"},
-];
+function Dialogs(props) {
+    let dialogsElement = props.state.dialogs.map( d => <DialogItem id={d.id} name={d.name} message={d.message}/>);
+    let messagesElement = props.state.message.map( m => <Message name={m.name} message={m.message}/>)
 
-let dialogsElement = dialogs.map( d => <DialogItem id={d.id} name={d.name} message={d.message}/>);
-let messagesElement = messages.map( m => <Message name={m.name} message={m.message}/>)
-
-function DialogItem(props) {
-    let path = "/dialogs/" + props.id;
-
-    return(
-        <div className="text-center">
-            <NavLink to={path}>
-                <div className="p-5 bg-white shadow-lg flex items-center space-x-4 w-full drop-shadow-md border-b-2 hover:bg-gray-200">
-                    <div className="shrink-0">
-                        <img className="h-12 w-12 rounded-full" src="https://cdnn1.img.sputniknews.com/img/102062/98/1020629890_18:0:6469:3648_1920x0_80_0_0_0d1dd4223c23b0a4c2e27b7f9d7f9b1c.jpg" alt="ChitChat Logo"/>
-                    </div>
-                    <div>
-                        <div className="text-xl font-medium text-black">{props.name}</div>
-                        <p className="text-slate-500">{props.message}</p>
-                    </div>
-                </div>
-            </NavLink>
-        </div>
-    );
-}
-
-function Message(props) {
-    return(
-        <div className="text-center p-2">
-            <div className="px-2 py-2 flex items-center space-x-4 w-full">
-                <div className="shrink-0">
-                    <img className="h-12 w-12 rounded-full" src="https://cdnn1.img.sputniknews.com/img/102062/98/1020629890_18:0:6469:3648_1920x0_80_0_0_0d1dd4223c23b0a4c2e27b7f9d7f9b1c.jpg" alt="ChitChat Logo"/>
-                </div>
-                <div>
-                    <div className="text-xl font-medium text-white text-left">{props.name}</div>
-                    <p className="text-gray-300">{props.message}</p>
-                </div>
-            </div>
-        </div>
-    );
-}
-
-function Dialogs() {
     return (
         <div className="container mx-auto mt-5 p-5 text-center h-screen">
             <div className="container mx-auto text-center mt-5">
