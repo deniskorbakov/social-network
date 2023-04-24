8d5b050ac81441876f4acdfc8159ae9a7cb9839d
deniskorbakov <korbakovd@gmail.com>
2023-04-24 14:35:47 +0300

    передал данные через пропсы

--- src/components/main/profile/Profile.jsx
+++ src/components/main/profile/Profile.jsx
@@ -3,13 +3,13 @@ import UserInfo from "./userInfo/UserInfo.jsx";
 import AddPost from "./addPost/AddPost.jsx";
 import Posts from "./posts/Posts.jsx";
 
-function Profile() {
+function Profile(props) {
     return (
         <div className="container mx-auto flex-grow">
             <Background/>
             <UserInfo/>
             <AddPost/>
-            <Posts/>
+            <Posts posts={props.state.posts}/>
         </div>
     );
 }
