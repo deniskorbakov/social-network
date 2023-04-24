8ac2468391bd4d5df7d9b706d89eb4d75ea58e49
deniskorbakov <korbakovd@gmail.com>
2023-04-24 14:35:58 +0300

    создал стейт данных

---
+++ src/redux/state.js
@@ -0,0 +1,33 @@
+let state = {
+    profilePage: {
+        posts : [
+            {name: "denis", message: "hello", like: 5},
+            {name: "denis", message: "hello", like: 5},
+            {name: "denis", message: "hello", like: 5},
+            {name: "denis", message: "hello", like: 5},
+            {name: "denis", message: "hello", like: 5},
+        ],
+    },
+
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: "denis", message: "hi"},
+            {id: 2, name: "dima", message: "hello"},
+            {id: 2, name: "dima", message: "hello"},
+            {id: 2, name: "dima", message: "hello"},
+            {id: 2, name: "dima", message: "hello"},
+            {id: 2, name: "dima", message: "hello"},
+        ],
+
+        message: [
+            {name: "denis", message: "hi"},
+            {name: "dima", message: "hello"},
+            {name: "dima", message: "hello"},
+            {name: "dima", message: "hello"},
+            {name: "dima", message: "hello"},
+            {name: "dima", message: "hello"},
+        ]
+    }
+}
+
+export default state;
